ce6ecb45fb7193a87220b3e9b8eb771e7fa4f055
CyberPunk <me@cyberpunk.eth>
2021-07-10 12:34:30 +0100

    version

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { NftFaucet } from './features/NftFaucet/NftFaucet';
 import { useAppSelector } from './app/hooks';
 import { selectConnected } from './features/connectWallet/connectWalletSlice';
 import { TrashCan } from './features/TrashCan/TrashCan';
+import { version } from '../package.json';
 
 function App() {
   // const dispatch = useAppDispatch();
@@ -19,6 +20,7 @@ function App() {
       <ConnectWallet></ConnectWallet>
       {connected ? <NftFaucet></NftFaucet> : null}
       {connected ? <TrashCan></TrashCan> : null}
+      <div className="version">{version}</div>
     </div>
   );
 }
